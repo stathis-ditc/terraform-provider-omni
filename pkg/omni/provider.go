b2215a51214d71a6c4f4b6dba826580a42c5b503
Stathis Kapaniaris <stathis.kapaniaris@devsinthe.cloud>
2025-04-20 19:26:46 +0200

    Add omni initial provider pkg

---
+++ pkg/omni/provider.go
@@ -0,0 +1,68 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package provider
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
+	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type omniProvider struct{}
+
+type omniProviderModel struct {
+	Endpoint          types.String `tfsdk:"endpoint"`
+	ServiceAccountKey types.String `tfsdk:service_account_key`
+}
+
+func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "omni"
+}
+
+func (p *omniProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"endpoint": schema.StringAttribute{
+				MarkdownDescription: "The Omni's API endpoint",
+				Optional:            false,
+			},
+			"service_account_key": schema.StringAttribute{
+				MarkdownDescription: "The generated base64 key of the service account created in Omni",
+				Optional:            true,
+			},
+		},
+	}
+}
+
+func (p *omniProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+
+}
+
+func (p *omniProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
+
+func (p *omniProvider) EphemeralResources(ctx context.Context) []func() ephemeral.EphemeralResource {
+	return []func() ephemeral.EphemeralResource{}
+}
+
+func (p *omniProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
+}
+
+func (p *omniProvider) Functions(ctx context.Context) []func() function.Function {
+	return []func() function.Function{}
+}
+
+func New() func() provider.Provider {
+	return &omniProvider{}
+}
